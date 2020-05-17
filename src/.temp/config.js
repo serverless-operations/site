dc12175084e73f7a7b543c62e320ffbfa6774671
Fumito Abe <abe@necco.co>
2020-05-17 19:00:19 +0900

    first commit

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - Serverless Operations",
-  "siteUrl": "",
-  "version": "0.7.14",
-  "catchLinks": true
-}
--- src/.temp/now.js
+++
@@ -1,1 +0,0 @@
-export default 1589709142693
