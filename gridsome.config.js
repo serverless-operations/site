b182a534dcc5f2ff457336a3a3f0954fc46ab0fd
Fumito Abe <abe@necco.co>
2020-05-17 19:31:07 +0900

    sass loader やvuetifyの追加してbuild動くとこまで

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,3 +1,18 @@
+const path = require('path')
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/scss/style.scss'),
+        path.resolve(__dirname, './src/assets/scss/*.scss'),
+      ],
+    })
+}
+
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
+
 module.exports = {
   siteName: 'Serverless Operations',
   siteDescription: 'Serverless Operationsは、これまでグローバルの第一線で培ってきたクラウド技術（AWS − アマゾンウェブサービス）の豊富な実績と知見を活かし、お客さまのサーバーレスによる開発や運用の支援、コンサルティングまで一貫してサポート。サーバーレスに関するさまざまな課題を解決いたします。',
@@ -16,5 +31,13 @@ module.exports = {
         typeName: 'WordPress', // GraphQL schema name (Optional)
       }
     }
-  ]
-}
+  ],
+
+  chainWebpack (config) {
+    // Load variables for all vue-files
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type => {
+      addStyleResource(config.module.rule('scss').oneOf(type))
+    })
+	}
+} // module.exports
---
+++ gridsome.server.js
@@ -0,0 +1,17 @@
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = function (api) {
+  api.chainWebpack((config, { isServer }) => {
+    if (isServer) {
+      config.externals([
+        nodeExternals({
+          whitelist: [/^vuetify/]
+        })
+      ])
+    }
+  })
+
+  api.loadSource(store => {
+    // Use the Data store API here: https://gridsome.org/docs/data-store-api
+  })
+}
