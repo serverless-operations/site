e86622bd0bac101deb20e12d2b96f1d9806d9c89
Fumito Abe <abe@necco.co>
2020-06-12 01:59:11 +0900

    パッケージ修正

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import 'vuetify/dist/vuetify.min.css'
 import DefaultLayout from '~/layouts/Default.vue'
 import Prism from 'prismjs'
 import '~/assets/css/prism-okaidia.css'
-import VueScrollReveal from 'gridsome-scroll-reveal';
+import GridsomeScrollReveal from 'gridsome-scroll-reveal';
 
-export default function (Vue, { router, head, appOptions }) {
+export default function (Vue, { router, head, appOptions, isClient, isServer }) {
   
   // Add an external Javascript before the closing </body> tag
   head.script.push({
@@ -47,5 +47,5 @@ export default function (Vue, { router, head, appOptions }) {
   })
 
   // Scroll Animation
-  Vue.use(VueScrollReveal);
+  Vue.use(GridsomeScrollReveal)
 }
