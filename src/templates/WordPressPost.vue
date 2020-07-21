e6682d062a5e9ac97cb032c859c593e1a6103e24
Fumito Abe <abe@necco.co>
2020-07-21 12:44:08 +0900

    feature image取得部分エラー修正

--- src/templates/WordPressPost.vue
+++ src/templates/WordPressPost.vue
@@ -127,7 +127,7 @@ export default {
         {
           key: `og:image`,
           property: `og:image`,
-          content:  this.$page.wordPressPost.featuredMedia.sourceUrl,
+          content: this.$page.metadata.siteOgImage,
         },
         {
           key: `og:title`,
