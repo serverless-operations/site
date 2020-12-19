1da1ba289b93f893df09301d80ce9de7b2e742d3
Fumito Abe <abe@necco.co>
2020-12-19 17:06:52 +0900

    ファイル作成してコンポーネント作成調整

---
+++ src/components/services/IotServicesFeatures.vue
@@ -0,0 +1,192 @@
+<template>
+  <div>
+    <section class="page-section-container">
+
+      <div class="container services-features-title-container">
+        <div class="row">
+          <h2 class="content-section-title text-center">
+            Features
+            <span>サーバーレスオペレーションズの強み</span>
+            <hr class="title-bottom-line">
+          </h2>
+        </div>
+      </div>
+
+      <v-container class="services-features-content-container">
+
+        <v-row justify="center">
+          <v-col cols="11" md="8" lg="8">
+            <h3 class="features-text-anim">
+              第一線で培った豊富な知見やノウハウを共有し、<br class="d-sm-block d-none" />お客さまと “共に” 課題を解決へと導きます。
+            </h3>
+          </v-col>
+        </v-row>
+
+        <v-row justify="center">
+          <v-col cols="11" offset-sm="3" sm="8" offset-md="3" md="6" offset-lg="5" lg="5">
+            <h4 class="features-text-anim">
+              “共創”とは、お客さまと“共につくる”こと。
+            </h4>
+            <p class="features-text-anim">私たちはこれまで、Serverless Frameworkを中心としたOSSコミュニティで、OSS開発やコントリビューションを続けてまいりました。グローバルの第一線で培ってきたサーバーレスアプリケーション開発に関する知見をお客さまに共有しながら、お客さまの現状やスケジュール、予算など、ご要望に合わせた支援・サポートを行います。</p>
+          </v-col>
+        </v-row>
+
+        <img
+          src="@/assets/images/services/features-illust01.svg"
+          class="features-illust-01"
+          alt=""
+        >
+        <img
+          src="@/assets/images/services/features-illust02.svg"
+          class="features-illust-02"
+          alt=""
+        >
+
+      </v-container>
+
+    </section>
+  </div>
+</template>
+
+<script>
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+export default {
+  mounted() {
+    this.FeaturesTitleAnimation(),
+    this.FeaturesAnimation()
+  },
+  methods: {
+    FeaturesTitleAnimation() {
+      let object = {
+        el: '.services-features-title-container h2',
+        duration: 1.2
+      }
+      gsap
+        .timeline({
+          defaults: { ease: 'Expo.easeInOut', duration: 1.2 }, // timelineのプロパティ
+          scrollTrigger: {
+            // markers: true, // マーカーを表示するか（開発用）
+            trigger: '.services-features-title-container', // この要素と交差するとイベントが発火
+            start: 'top 98%', // ウィンドウのどの位置を発火の基準点にするか
+            end: 'bottom 10%', // ウィンドウのどの位置をイベントの終了点にするか
+            toggleActions: 'play none none none', // スクロールイベントで発火するアニメーションの種
+          },
+        })
+        .fromTo( object.el, {
+          opacity: 0,
+          y: 0,
+          scale: 0.98,
+        }, {
+          opacity: 1,
+          scale: 1,
+          y: -20,
+        })
+        .fromTo('.title-bottom-line', {
+          width: 0,
+        }, {
+          width: 80,
+        },'<')
+    },
+    FeaturesAnimation() {
+      gsap.timeline({
+        scrollTrigger: {
+          // markers: true, // マーカーを表示するか（開発用）
+          // scrub: 1,
+          trigger: '.services-features-content-container', // この要素と交差するとイベントが発火
+          start: 'top 95%', // ウィンドウのどの位置を発火の基準点にするか
+          end: 'bottom 10%', // ウィンドウのどの位置をイベントの終了点にするか
+          toggleActions: 'play none none none', // スクロールイベントで発火するアニメーションの種
+        },
+      })
+      .fromTo( '.features-text-anim', {
+        opacity: 0,
+        y: 0,
+      }, {
+        delay: 0.3,
+        ease: 'Expo.easeInOut',
+        transformOrigin: '0 1',
+        y: -16,
+        duration: 1,
+        opacity: 1,
+        stagger: {
+          from: 'start', //左側から
+          axis: 'x',
+          amount: 0.2 // 0.8秒おきに
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@include SectionTitle;
+
+.page-section-container {
+  padding: 160px 0;
+  background-image: url('../../assets/images/bg/features-dot-texture.svg');
+  background-position: top 420px;
+  background-size: 200%;
+  background-repeat: repeat-y;
+  background-color: $light-gray-2;
+  @include media-breakpoint-down(sm) {
+    background-size: 180%;
+  }
+  h3 {
+    color: $secondary;
+    font-family: $font-jp-bold;
+    font-size: 32px;
+    margin-bottom: 48px;
+    @include media-breakpoint-down(md) {
+      margin-bottom: 24px;
+      font-size: 20px;
+    }
+  }
+  h4 {
+    color: $text-black;
+    font-size: 24px;
+    font-family: $font-jp-bold;
+    letter-spacing: -1px;
+    margin-bottom: 16px;
+    @include media-breakpoint-down(md) {
+      font-size: 20px;
+    }
+  }
+  p {
+    color: $text-black;
+    font-family: $font-jp-regular;
+    line-height: 32px;
+    text-align: justify;
+  }
+}
+
+.services-features-content-container {
+  position: relative;
+  .features-illust-01 {
+    position: absolute;
+    left: 0;
+    bottom: -80px;
+    width: 70%;
+    max-width: 552px;
+    @include media-breakpoint-down(md) {
+      bottom: -100px;
+      width: 50%;
+    }
+  }
+  .features-illust-02 {
+    position: absolute;
+    right: -100px;
+    top: -200px;
+    width: 70%;
+    max-width: 552px;
+    @include media-breakpoint-down(md) {
+      right: -40px;
+      bottom: -120px;
+      width: 50%;
+    }
+  }
+}
+</style>
---
+++ src/pages/services/IotServerless.vue
@@ -0,0 +1,173 @@
+<template>
+  <Layout>
+    <div class="v-content__wrap">
+
+      <div class="l-about-page">
+
+        <div class="page-header-container">
+        
+          <div class="page-header-img-container">
+            <!--背景 -->
+            <img src="@/assets/images/about-so-main-bg.svg"
+                class="page-header-main-bg d-none d-md-block"  alt="Serverless Operationsとは メインイメージ">
+            <!-- ロゴ -->
+            <img src="@/assets/images/about-so-main-logo.svg"
+                class="page-header-logo d-none d-md-block" alt="Serverless Operationsとは メインイメージ ロゴ">
+            <!-- 粒子 -->
+            <img src="@/assets/images/about-so-main-square.svg"
+                class="page-header-main-particle d-none d-md-block" alt="Serverless Operationsとは メインイメージ">
+
+            <!--背景 -->
+            <img src="@/assets/images/about-so-main-bg-sp.svg"
+                class="page-header-main-bg d-md-none" alt="Serverless Operationsとは メインイメージ">
+            <!-- ロゴ -->
+            <img src="@/assets/images/about-so-main-logo-sp.svg"
+                class="page-header-logo-sp d-md-none" alt="Serverless Operationsとは メインイメージ ロゴ">
+            <!-- 粒子 -->
+            <img src="@/assets/images/about-so-main-particle-sp.svg"
+                class="page-header-main-particle d-md-none" alt="Serverless Operationsとは メインイメージ">
+          </div>
+
+          <v-container class="page-header-text-container">
+            <v-row>
+              <v-col cols="12" md="10">
+                <h1 class="page-header-title text-anim">
+                  <span class="text-anim">IoT × SERVERLESS</span> 
+                  <span class="page-header-title-jp text-anim">
+                    IoT × サーバーレス / コンテナ開発導⼊サポート
+                  </span>
+                  <span class="page-title-anim-bg"></span>
+                </h1>
+                <h2 class="page-header-copy page-header-text-anim">
+                  AWSを使ったIoTの<br class="d-md-block d-none" />バックエンドシステム開発をサポート
+                </h2>
+                <p class="page-header-text page-header-text-anim">
+                  サーバーレスやコンテナなどのクラウドネイティブな技術を使⽤することで、<br class="d-md-block d-none" />システム運⽤のリソースやコスト最適化などが実現できます。<br class="d-md-block d-none" />サーバーレス/コンテナのノウハウをプロジェクトを通してお伝えし、<br class="d-md-block d-none" />開発の内製化までをサポートします。
+                </p>
+              </v-col>
+            </v-row>
+          </v-container>
+
+        </div><!-- p-services-header -->
+        
+        <IotServicesSupport />
+
+        <IotServicesMerit />
+
+        <IotServicesFeatures />
+
+        <IotServicesUseCase />
+       
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import IotServicesSupport from '~/components/services/IotServicesSupport'
+import IotServicesMerit from '~/components/services/IotServicesMerit'
+import IotServicesFeatures from '~/components/services/IotServicesFeatures'
+import IotServicesUseCase from '~/components/services/IotServicesUseCase'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+export default {
+  components: {
+    IotServicesSupport,
+    IotServicesMerit,
+    IotServicesFeatures,
+    IotServicesUseCase
+  },
+  metaInfo () {
+    return {
+      title: 'サーバーレスオペレーションズのサービスと強み',
+      description: 'サーバーレスとは、付加価値を産まない重労働をクラウドへアウトソースし、開発者がビジネスのコアバリューに集中するための クラウドの利用方法であり開発のための手段です。'
+    }
+  },
+  fetch({ store }) {
+    store.commit('resetModal')
+  },
+  mounted() {
+    this.PageTextAnimation()
+  },
+  methods: {
+    PageTextAnimation() {
+      gsap.fromTo('.page-title-anim-bg', {
+        opacity: 1,
+        scaleX: 0,
+        transformOrigin: '0 0',
+      }, {
+        ease: 'Expo.easeInOut',
+        transformOrigin: '1 0',
+        scaleX: 1,
+        duration: 1.6,
+        opacity: 0,
+        stagger: {
+          from: 'start', //左側から
+          axis: 'x',
+          amount: 0.4 // 0.8秒おきに
+        }
+      })
+      gsap.fromTo('.text-anim', {
+        opacity: 0,
+        x: -32,
+      }, {
+        delay: 0.2,
+        ease: 'Expo.easeInOut',
+        transformOrigin: '1 0',
+        x: 0,
+        duration: 1.8,
+        opacity: 1,
+        stagger: {
+          from: 'start', //左側から
+          axis: 'x',
+          amount: 0.4 // 0.8秒おきに
+        }
+      })
+      gsap.fromTo('.page-header-text-anim', {
+        opacity: 0,
+        y: 0,
+      }, {
+        delay: 1.4,
+        ease: 'Expo.easeInOut',
+        transformOrigin: '0 1',
+        y: -16,
+        duration: 1,
+        opacity: 1,
+        stagger: {
+          from: 'start', //左側から
+          axis: 'x',
+          amount: 0.2 // 0.8秒おきに
+        }
+      })
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.v-content__wrap {
+  background-image: url('../../assets/images/bg/noise.png');
+  overflow: hidden;
+}
+.l-bg-gray {
+  background: $light-gray-2;
+}
+.l-about-page {
+  background: $white;
+  position: relative;
+  overflow: hidden;
+}
+.l-services-strength {
+  position: relative;
+}
+.page-header-container {
+background:
+  url('../../assets/images/bg/so-blue-texture.png'),
+  url('../../assets/images/company-main-pattern.svg'),
+  linear-gradient(45deg, $secondary 11.76%, $tertiary 88.21%);
+}
+@include PageHeader;
+
+</style>
