c541d1d90ceb5c45f2a4e4c315cae6917030005d
lopburny <lopburny@riotz.works>
2021-02-22 07:54:08 +0000

    Remove chatplus

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -183,16 +183,6 @@ export default {
   }
 }
 
-if (process.client) {
-  (function(){
-  var w=window,d=document;
-  var s="https://app.chatplus.jp/cp.js";
-  d["__cp_d"]="https://app.chatplus.jp";
-  d["__cp_c"]="35c32da0_1";
-  var a=d.createElement("script"), m=d.getElementsByTagName("script")[0];
-  a.async=true,a.src=s,m.parentNode.insertBefore(a,m);})();
-}
-
 </script>
 
 <style lang="scss" scoped>
